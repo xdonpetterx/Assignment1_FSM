8d2f317c186b4270f29c94c0e9857281e57006f3
xdonpetterx <donpetter@gmail.com>
2024-06-17 19:39:32 +0200

    Update main.cpp for enhanced character handling
    The update includes code for creating characters as pointers and registering them with the entity manager. This update also introduces a random number generator seeding, and ensures that any delayed messages are dispatched. Thus, the character and messaging handling mechanisms are made more robust and efficient.

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,28 +1,45 @@
-#include <Characters/Agent.h>
-#include <Characters/EntityNames.h>
-
 #include <iostream>
+#include <fstream>
+#include <ctime>
 #include <chrono>
 #include <thread>
 
+#include <Characters/Agent.h>
+#include <Characters/EntityNames.h>
+#include <Messaging/EntityManager.h>
+#include <Messaging/MessageDispatcher.h>
+
+
 int main()
 {
+    //seed random number generator
+    srand((unsigned) time(nullptr));
+
     //Creates the main character and his friends
-    Agent Charlie(ent_Agent_Charlie);
-    Agent Friend_Elena(ent_Friend_Elena);
-    Agent Friend_Jonny(ent_Friend_Jonny);
-    Agent Friend_Mike(ent_Friend_Mike);
+    auto* Charlie = new Agent(ent_Agent_Charlie);
+    auto* Friend_Elena = new Agent(ent_Friend_Elena);
+    auto* Friend_Jonny = new Agent(ent_Friend_Jonny);
+    auto* Friend_Mike = new Agent(ent_Friend_Mike);
+
+    //register them with the entity manager
+    EntityMgr->RegisterEntity(Charlie);
+    EntityMgr->RegisterEntity(Friend_Elena);
+    EntityMgr->RegisterEntity(Friend_Jonny);
+    EntityMgr->RegisterEntity(Friend_Mike);
 
     for (int i=0; i<20; ++i)
     {
-        Charlie.Update();
+        Charlie->Update();
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        Friend_Elena.Update();
+        Friend_Elena->Update();
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        Friend_Jonny.Update();
+        Friend_Jonny->Update();
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        Friend_Mike.Update();
+        Friend_Mike->Update();
         std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+        //dispatch any delayed messages
+        Dispatch->DispatchDelayedMessages();
     }
 
     //wait for a keypress before exiting
