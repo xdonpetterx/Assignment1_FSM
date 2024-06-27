86e657f2a8eecf7c38a02bb4d39b2a6992147447
xdonpetterx <donpetter@gmail.com>
2024-06-27 19:25:23 +0200

    Add more location types to Locations.h
    The update expands the enumeration of location types in Locations.h. The added locations are mostly workplaces, categorized under different names, along with a category for other locations. The aim is to broaden the context of the program and allow for more diverse implementations.

--- Code/Locations/Locations.h
+++ Code/Locations/Locations.h
@@ -3,7 +3,20 @@
 enum location_type
 {
     AgentHouse,
+    //WORKPLACES
+    //Charlie
     McDonalds,
+    CoalMine,
+    //Elena
+    PowerPlant,
+    ChemicalFactory,
+    //Jonny
+    Hospital,
+    CarFactory,
+    //Mike
+    Pizzeria,
+    Prison,
+    //OTHER_LOCATIONS
     Bank,
     Bar,
     Walmart
